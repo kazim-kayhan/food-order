0d53c58cc1083e5d02e0f177d65d9ca699360bf8
Kazim Kayhan <kazimmohammadi400@gmail.com>
2022-06-27 09:28:39 +0430

    Fetch data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,64 @@
+import React, { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-import DUMMY_MEALS from "./DUMMY_MEALS.json";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-13bf4-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const result = await response.json();
+      const loadedMeals = [];
+      for (const key in result) {
+        loadedMeals.push({
+          id: key,
+          name: result[key].name,
+          description: result[key].description,
+          price: result[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.loading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.error}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  if (meals.length === 0) {
+    return (
+      <section className={classes.error}>
+        <p>Data not found!</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
 
   return (
     <section className={classes.meals}>
